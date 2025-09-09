269fa407f65a33dc9d837252491e6b74e8d7dd67
Marzieh Mohamadi <mohamadimarzieh513@gmail.com>
2025-09-09 14:10:01 -0800

    test(WeatherService): add unit tests for GetWeatherAsync method

--- WeatherApi.Tests/UnitTest1.cs
+++ WeatherApi.Tests/UnitTest1.cs
@@ -1,13 +1,8 @@
 using Xunit;
 using Moq;
-using Moq.Protected;
-using Microsoft.Extensions.Configuration;
-using System.Net;
-using System.Net.Http;
-using System.Threading;
+using WiraSystem_Assessment.Infrastructure.Services;
+using WiraSystem_Assessment.Application.Interfaces;
 using System.Threading.Tasks;
-using System.Collections.Generic;
-using WeatherApi.Services; // این خط مهم است
 
 namespace WeatherApi.Tests;
 
@@ -17,32 +12,14 @@ public class WeatherServiceTests
     public async Task GetWeatherAsync_Should_Return_Null_When_City_Not_Found()
     {
         // Arrange
-        var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+        var mockOpenWeatherClient = new Mock<IOpenWeatherMapClient>();
         
-        // Setup mock HTTP response
-        mockHttpMessageHandler.Protected()
-            .Setup<Task<HttpResponseMessage>>(
-                "SendAsync",
-                ItExpr.IsAny<HttpRequestMessage>(),
-                ItExpr.IsAny<CancellationToken>()
-            )
-            .ReturnsAsync(new HttpResponseMessage {
-                StatusCode = HttpStatusCode.NotFound,
-                Content = new StringContent("")
-            });
+        // وقتی شهر پیدا نشه، null برمی‌گردونه
+        mockOpenWeatherClient
+            .Setup(x => x.GetWeatherDataAsync("InvalidCityName12345"))
+            .ReturnsAsync((WeatherData)null); // یا throws مناسب
 
-        var httpClient = new HttpClient(mockHttpMessageHandler.Object);
-        
-        var inMemorySettings = new Dictionary<string, string> {
-            {"OpenWeatherMap:ApiKey", "fake-key"},
-            {"OpenWeatherMap:BaseUrl", "https://api.openweathermap.org/data/2.5/"}
-        };
-        
-        IConfiguration configuration = new ConfigurationBuilder()
-            .AddInMemoryCollection(inMemorySettings)
-            .Build();
-
-        var service = new WeatherService(httpClient, configuration);
+        var service = new WeatherService(mockOpenWeatherClient.Object);
 
         // Act
         var result = await service.GetWeatherAsync("InvalidCityName12345");
--- obj/Debug/net8.0/WiraSystem-Assessment.AssemblyInfo.cs
+++ obj/Debug/net8.0/WiraSystem-Assessment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WiraSystem-Assessment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87267f0e192f7bd765fca5e48cb3ab2c97e3c1e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0f0a67770b66b81af00200fd243b36380ae854d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WiraSystem-Assessment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WiraSystem-Assessment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
